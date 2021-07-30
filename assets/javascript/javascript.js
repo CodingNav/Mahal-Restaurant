97c882ebd842ddf00b1ee00bb756240bf1d57b92
CodingNav <navleenkaurr@gmail.com>
2021-07-30 16:52:22 -0400

    Added eventListener for form and set up the API request

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,26 +1,26 @@
 //MENU
-$(document).ready(function() {
+$(document).ready(function () {
 
-    function toggleSidebar() {
-      $(".button").toggleClass("active");
-      $(".sidebar, .nav-right").toggleClass("move-to-left");
-      $(".sidebar-item").toggleClass("active");
-    }
-  
-    $(".button").on("click tap", function() {
-      toggleSidebar();
-    });
-  
-    $(document).keyup(function(e) {
-      if (e.keyCode === 27) {
-        toggleSidebar();
-      }
-    });
-  
+  function toggleSidebar() {
+    $(".button").toggleClass("active");
+    $(".sidebar, .nav-right").toggleClass("move-to-left");
+    $(".sidebar-item").toggleClass("active");
+  }
+
+  $(".button").on("click tap", function () {
+    toggleSidebar();
   });
 
+  $(document).keyup(function (e) {
+    if (e.keyCode === 27) {
+      toggleSidebar();
+    }
+  });
+
+});
+
 if (document.querySelector(".mySwiper")) {
-   // Home Page Image Carousel Initializer
+  // Home Page Image Carousel Initializer
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     slidesPerColumn: 1,
@@ -46,4 +46,35 @@ if (document.querySelector(".mySwiper")) {
     },
   });
 }
- 
+
+var contactForm = document.querySelector("#contact-form");
+
+contactForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var nameInput = document.querySelector("#name-input");
+  var emailInput = document.querySelector("#email-input");
+  var numberInput = document.querySelector("#number-input");
+  var subjectInput = document.querySelector("#subject-input");
+  var messageInput = document.querySelector("#message-input");
+
+  var emailData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    number: numberInput.value,
+    subject: subjectInput.value,
+    message: messageInput.value
+  }
+
+  fetch('https://mahal-email.herokuapp.com/api/send-email', {
+    method: 'post',
+    body: JSON.stringify(emailData),
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    }
+  }).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    console.log(data);
+  });
+});
